a714c644032e1e2665354ed67380b329263be215
FirzaFahrezi <heni.130780@gmail.com>
2022-10-19 20:04:35 +0800

    E1E121030_Laode Muhammad Firza Fahrezi-Add Light

--- main.cpp
+++ main.cpp
@@ -28,6 +28,26 @@
      return 0;
     }
 =======
+ //La Ode Muhammad Firza Fahrezi
+     //Add ambient light
+     GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color (0.2, 0.2, 0.2)
+     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+     //Add positioned light
+     GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
+     GLfloat lightPos0[] = {0.0f, -8.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+     glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+     //Add directed light
+     GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
+     GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
+     glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
+     glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
+     glRotatef(10, 1.0f, 0.0f, 0.0f);
+     glRotatef(-0.0, 0.0f, 0.0f, 1.0f);
+     glRotatef(_angle,0.0f, 1.0f, 0.0f);
+     glColor3f(1.1f, 1.1f, 1.1f);
+     glBegin(GL_QUADS);
+
      // E1E121036_NIRWANA
      //front depan atas
      glColor3f(5.1f, 5.1f, 1.1f);
